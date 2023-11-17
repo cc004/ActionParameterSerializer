cd57089ae5ea7d565f8c03365991364480f42c55
cc004 <1176321897@qq.com>
2023-11-18 03:00:07 +0800

    fix typo

--- Actions/ChangeCriticalReferenceAction.cs
+++ Actions/ChangeCriticalReferenceAction.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel;
 using Newtonsoft.Json.Linq;
 
 namespace ActionParameterSerializer.Actions;
@@ -24,6 +25,8 @@ public class ChangeCriticalReferenceAction : ActionParameter
 
     public override string localizedDetail(int level, Property property)
     {
+        if (refType == eCriticalReference.normal)
+            return Utils.GetString("no_effect");
         return Utils.JavaFormat(Utils.GetString("Use_critical_reference_s1_for_skill_d2"),
             refType.rawDescription(),
             actionId);

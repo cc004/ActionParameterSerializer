ad22d00415180d5de0193a44db6687ee00d3dedf
krulci <52474054+krulci@users.noreply.github.com>
2023-11-22 11:00:36 -0500

    Updated ModeChangeAction

--- Actions/ModeChangeAction.cs
+++ Actions/ModeChangeAction.cs
@@ -16,12 +16,28 @@ public class ModeChangeAction : ActionParameter
         release = 3
     }
 
+    public enum eAdditionalAbnormalType
+    {
+        NONE,
+        FLIGHT
+    }
+
+    public enum eCancelTriggerType
+    {
+        NONE,
+        UNABLE_ABNORMAL_OR_CHARM
+    }
+
     private ModeChangeType modeChangeType;
+    private eAdditionalAbnormalType additionalAbnormalType;
+    private eCancelTriggerType cancelTriggerType;
 
     public
     override void childInit()
     {
         modeChangeType = (ModeChangeType)(actionDetail1);
+        additionalAbnormalType = (eAdditionalAbnormalType)(actionValue5.value);
+        cancelTriggerType = (eCancelTriggerType)(actionValue6.value);
     }
 
     public
@@ -30,8 +46,8 @@ public class ModeChangeAction : ActionParameter
         switch (modeChangeType)
         {
             case ModeChangeType.time:
-                return Utils.JavaFormat(Utils.GetString("Change_attack_pattern_to_d1_for_s2_sec"),
-                        actionDetail2 % 10, actionValue1.valueString());
+                return Utils.JavaFormat(Utils.GetString("Change_attack_pattern_to_d1_for_s2_sec_set_abnormal_state_s3_to_self"),
+                        actionDetail2 % 10, actionValue1.valueString(), additionalAbnormalType.GetDescription());
             case ModeChangeType.energy:
                 return Utils.JavaFormat(Utils.GetString("Cost_s1_TP_sec_change_attack_pattern_to_d2_until_TP_is_zero"),
                         Utils.roundDownDouble(actionValue1.value), actionDetail2 % 10);
